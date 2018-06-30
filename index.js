2de46639e9aab39db9ef2a72c9934b0d1a462a83
爬上上坡 <pashangshangpo@gmail.com>
2018-06-30 09:06:05 +0800

    获取周keys

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const Storage = require('json-storage')
 
 const getDateTime = () => {
   let date = new Date()
-  let day = new Date(date.getFullYear(), date.getMonth() + 1, date.getDate())
+  let today = new Date(date.getFullYear(), date.getMonth() + 1, date.getDate())
 
-  return day.getTime()
+  return today.getTime()
 }
 
 class StorageDate extends Storage {
@@ -28,6 +28,28 @@ class StorageDate extends Storage {
 
   remove(key = getDateTime()) {
     return super.remove(key)
+  }
+
+  getWeekKeys() {
+    let date = new Date()
+    let day = date.getDay()
+
+    if (day === 0) {
+      day = 7
+    }
+
+    let dayTime = new Date(date.getFullYear(), date.getMonth() + 1, date.getDate() - day).getTime()
+    let week = new Date(dayTime)
+
+    week.setDate(week.getDate() + 7)
+
+    let weekTime = week.getTime()
+
+    return super.findKeys(function(key) {
+      let date = parseFloat(key)
+
+      return date >= dayTime && date <= weekTime
+    })
   }
 }
 
