ccfce9fb7ddff2152a5aa3b9a946725b73562d87
爬上上坡 <pashangshangpo@gmail.com>
2018-06-30 08:31:32 +0800

    根据天来设置

--- index.js
+++ index.js
@@ -4,3 +4,19 @@
  * @date 2018-06-30 08:22:06
  */
 
+const Storage = require('json-storage')
+
+class StorageDate extends Storage {
+  constructor(root) {
+    super(root)
+  }
+
+  set(data) {
+    let date = new Date()
+    let day = new Date(date.getFullYear(), date.getMonth() + 1, date.getDate())
+
+    super.set(day.getTime(), data)
+  }
+}
+
+module.exports = StorageDate
